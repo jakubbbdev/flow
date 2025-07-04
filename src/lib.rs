1d9d398a0a1a2c6d0519393b59d3831b6185ee73
jakubbbbdev <javainterfacejava@gmail.com>
2025-07-05 01:26:46 +0200

    Refactor: Migrate project to library, update edition to 2024, and configure dependencies with feature flags

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[cfg(feature = "flow_store")]
+pub mod flow_store;
+
+#[cfg(feature = "flow_queue")]
+pub mod flow_queue;
+
+#[cfg(feature = "flow_definition")]
+pub mod flow_definition;
+
+#[cfg(feature = "flow_config")]
+pub mod flow_config;
